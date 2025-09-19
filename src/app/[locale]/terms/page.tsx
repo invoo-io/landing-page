e6863b0fab84afcd70f075e6e45771e6b8e2a177
thishonestabe <marquez.abraham1991@gmail.com>
2025-09-19 23:34:14 +0200

    feat: compliance pages

---
+++ src/app/[locale]/terms/page.tsx
@@ -0,0 +1,21 @@
+import { setRequestLocale } from "next-intl/server";
+import Navigation from "@/components/Navigation";
+import TermsContent from "@/components/TermsContent";
+import Footer from "@/components/Footer";
+
+export default async function TermsPage({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
+  setRequestLocale(locale);
+
+  return (
+    <div className="min-h-screen bg-black">
+      <Navigation locale={locale} />
+      <TermsContent />
+      <Footer />
+    </div>
+  );
+}
